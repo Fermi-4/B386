0c4323189af1f3eda52dc3e871c426146d719d50
fermi-4 <fermiparadox4@gmail.com>
2019-03-27 23:02:04 -0500

    init

---
+++ src/main/java/com/test/Boostrap386Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boostrap386Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boostrap386Application.class, args);
+	}
+
+}
